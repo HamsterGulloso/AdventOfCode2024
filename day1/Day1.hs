867e67b6a5d0c713c9107387b8ca664d52d41d71
HamsterGulloso <danielgullovasconcelos@gmail.com>
2025-10-07 15:26:39 -0300

    Day 1

--- day1/Day1.hs
+++ day1/Day1.hs
@@ -1,26 +1,21 @@
 import qualified Data.List as List
 import Text.Printf (printf)
 
-readLists = do
-    line <- getLine
-    case line of
-        "" -> return ([], [])
-        l -> 
-            let 
-                w = map (read :: String -> Int) (words l)
-                nextLine = readLists
-            in
-            nextLine >>= \l ->
-                let (l1, l2) = l in
-                return (w!!0 : l1, w!!1 : l2)
-
+makeLists [] = ([], [])
+makeLists (h1:h2:rest)  =
+    let (t1, t2) = makeLists rest in
+    (h1:t1, h2:t2)
+        
 
 main = do
-    lists <- readLists
+    line <- readFile "./input.txt"
+    let w = words line 
+    let (l1, l2) = makeLists w
     let 
-        (l1, l2) = lists
-        ol1 = List.sort l1
-        ol2 = List.sort l2
+        l1_int = map (read :: String -> Int) l1
+        l2_int = map (read :: String -> Int) l2
+        ol1 = List.sort l1_int
+        ol2 = List.sort l2_int
         sum = foldr (\(e1, e2) acc -> acc + abs (e1 - e2)) 0 (zip ol1 ol2)
     printf "Sum: %d\n" sum
 
